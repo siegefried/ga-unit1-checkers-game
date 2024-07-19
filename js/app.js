b6d95b75407f339176018ec197ef1cead23309ff
Josh Lim <justdoit.josh@gmail.com>
2024-07-19 21:26:03 +0800

    feat: added initial evaluation function todo: remove moveSelections if jump is available

--- js/app.js
+++ js/app.js
@@ -55,15 +55,25 @@ const render = () => {};
 const initBoard = () => {
   game.board.length = 0;
   for (i = 0; i < 12; i++) {
-    game.board.push("");
-    game.board.push(i);
+    if (i > 3 && i < 8) {
+      game.board.push(i);
+      game.board.push("");
+    } else {
+      game.board.push("");
+      game.board.push(i);
+    }
   }
   for (i = 0; i < 16; i++) {
     game.board.push("");
   }
   for (i = 12; i < 24; i++) {
-    game.board.push(i);
-    game.board.push("");
+    if (i > 15 && i < 20) {
+      game.board.push("");
+      game.board.push(i);
+    } else {
+      game.board.push(i);
+      game.board.push("");
+    }
   }
 };
 
@@ -107,9 +117,174 @@ const init = () => {
   game.win = false;
 };
 
+//Fwd is moving up the screen from perspective of playerOne
+const evalPlayerOneFwdMoves = () => {
+  for (const checker of game.playerOneCheckerObjs) {
+    const leftJumpIndex = checker.boardIndex - 18;
+    const leftMoveIndex = checker.boardIndex - 9;
+    const rightJumpIndex = checker.boardIndex - 14;
+    const rightMoveIndex = checker.boardIndex - 7;
+    if (
+      game.board[leftJumpIndex] === "" &&
+      game.board[leftMoveIndex] < 12 &&
+      game.board[leftMoveIndex] !== "" &&
+      cellEls[leftJumpIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.minusEighteen = true;
+    }
+    if (
+      game.board[rightJumpIndex] === "" &&
+      game.board[rightMoveIndex] < 12 &&
+      game.board[rightMoveIndex] !== "" &&
+      cellEls[rightJumpIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.minusForteen = true;
+    }
+    if (
+      game.board[leftMoveIndex] === "" &&
+      cellEls[leftMoveIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.minusNine = true;
+    }
+    if (
+      game.board[rightMoveIndex] === "" &&
+      cellEls[rightMoveIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.minusSeven = true;
+    }
+  }
+};
+
+//Bwd is moving down the screen from perspective of playerOne
+const evalPlayerOneBwdMoves = (checker) => {
+  const bwdLeftJumpIndex = checker.boardIndex + 14;
+  const bwdLeftMoveIndex = checker.boardIndex + 7;
+  const bwdRightJumpIndex = checker.boardIndex + 18;
+  const bwdRightMoveIndex = checker.boardIndex + 9;
+  if (
+    game.board[bwdLeftJumpIndex] === "" &&
+    game.board[bwdLeftMoveIndex] < 12 &&
+    game.board[bwdLeftMoveIndex] !== "" &&
+    cellEls[bwdLeftJumpIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.plusFourteen = true;
+  }
+  if (
+    game.board[bwdRightJumpIndex] === "" &&
+    game.board[bwdRightMoveIndex] < 12 &&
+    game.board[bwdRightMoveIndex] !== "" &&
+    cellEls[bwdRightJumpIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.plusEighteen = true;
+  }
+  if (
+    game.board[bwdLeftMoveIndex] === "" &&
+    cellEls[bwdLeftMoveIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.plusSeven = true;
+  }
+  if (
+    game.board[bwdRightMoveIndex] === "" &&
+    cellEls[bwdRightMoveIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.plusNine = true;
+  }
+};
+
+const evalPlayerTwoBwdMoves = () => {
+  for (const checker of game.playerTwoCheckerObjs) {
+    const bwdLeftJumpIndex = checker.boardIndex + 14;
+    const bwdLeftMoveIndex = checker.boardIndex + 7;
+    const bwdRightJumpIndex = checker.boardIndex + 18;
+    const bwdRightMoveIndex = checker.boardIndex + 9;
+    if (
+      game.board[bwdLeftJumpIndex] === "" &&
+      game.board[bwdLeftMoveIndex] > 11 &&
+      game.board[bwdLeftMoveIndex] !== "" &&
+      cellEls[bwdLeftJumpIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.plusFourteen = true;
+    }
+    if (
+      game.board[bwdRightJumpIndex] === "" &&
+      game.board[bwdRightMoveIndex] > 11 &&
+      game.board[bwdRightMoveIndex] !== "" &&
+      cellEls[bwdRightJumpIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.plusEighteen = true;
+    }
+    if (
+      game.board[bwdLeftMoveIndex] === "" &&
+      cellEls[bwdLeftMoveIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.plusSeven = true;
+    }
+    if (
+      game.board[bwdRightMoveIndex] === "" &&
+      cellEls[bwdRightMoveIndex].classList.contains("unusedCell") === false
+    ) {
+      checker.plusNine = true;
+    }
+  }
+};
+
+const evalPlayerTwoFwdMoves = (checker) => {
+  const leftJumpIndex = checker.boardIndex - 18;
+  const leftMoveIndex = checker.boardIndex - 9;
+  const rightJumpIndex = checker.boardIndex - 14;
+  const rightMoveIndex = checker.boardIndex - 7;
+  if (
+    game.board[leftJumpIndex] === "" &&
+    game.board[leftMoveIndex] > 11 &&
+    game.board[leftMoveIndex] !== "" &&
+    cellEls[leftJumpIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.minusEighteen = true;
+  }
+  if (
+    game.board[rightJumpIndex] === "" &&
+    game.board[rightMoveIndex] > 11 &&
+    game.board[rightMoveIndex] !== "" &&
+    cellEls[rightJumpIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.minusForteen = true;
+  }
+  if (
+    game.board[leftMoveIndex] === "" &&
+    cellEls[leftMoveIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.minusNine = true;
+  }
+  if (
+    game.board[rightMoveIndex] === "" &&
+    cellEls[rightMoveIndex].classList.contains("unusedCell") === false
+  ) {
+    checker.minusSeven = true;
+  }
+};
+
+const evalPossibleMoves = () => {
+  if (game.turn) {
+    evalPlayerOneFwdMoves();
+    for (const checker of game.playerOneCheckerObjs) {
+      if (checker.isKing) {
+        evalPlayerOneBwdMoves(checker);
+      }
+    }
+  } else {
+    evalPlayerTwoBwdMoves();
+    for (const checker of game.playerTwoCheckerObjs) {
+      if (checker.isKing) {
+        evalPlayerTwoFwdMoves(checker);
+      }
+    }
+  }
+};
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 init();
+evalPossibleMoves();
 
 //move id15 to cell 39
 
